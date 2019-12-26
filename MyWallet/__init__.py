31bf29e6cf0bbae67eedeebf091a1aa3f1778ac0
denefim <denefim@gmail.com>
2019-12-27 01:11:13 +0200

    tests update

--- MyWallet/__init__.py
+++ MyWallet/__init__.py
@@ -1,2 +1,17 @@
-__version__ = '0.1.3'
-VERSION = __version__
+from datetime import date
+
+from MyWallet.models import Account, Split, Transaction
+
+
+def create_transaction(title, src, dst, amount, type, date=date.today(), category=None):
+    t = Transaction.objects.create(title=title, date=date, transaction_type=type)
+    Split.objects.bulk_create([
+        Split(title=title, account=src, opposing_account=dst,
+              amount=-amount, transaction=t, date=date, category=category),
+        Split(title=title, account=dst, opposing_account=src,
+              amount=amount, transaction=t, date=date, category=category)])
+    return t
+
+
+def create_account(name, account_type=Account.PERSONAL):
+    return Account.objects.create(name=name, account_type=account_type)
